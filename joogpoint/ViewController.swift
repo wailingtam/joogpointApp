c8c914eb0dc43ad5f66c1e0619569a6f27ffcf78
Wai Ling Tam <wai.ling.tam@est.fib.upc.edu>
2016-05-27 14:26:00 +0200

    Initial Commit

---
+++ joogpoint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  joogpoint
+//
+//  Created by Wai Ling Tam on 27/5/16.
+//  Copyright © 2016 Wai Ling Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ joogpointTests/joogpointTests.swift
@@ -0,0 +1,36 @@
+//
+//  joogpointTests.swift
+//  joogpointTests
+//
+//  Created by Wai Ling Tam on 27/5/16.
+//  Copyright © 2016 Wai Ling Tam. All rights reserved.
+//
+
+import XCTest
+@testable import joogpoint
+
+class joogpointTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
